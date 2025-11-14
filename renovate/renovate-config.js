2453cd959b049ba847864f78cb68ea2e9f050afc
BenyBoy84 <Blais_Benoit@hotmail.com>
2025-11-14 14:24:43 -0500

    repo

--- renovate/renovate-config.js
+++ renovate/renovate-config.js
@@ -1,4 +1,17 @@
 module.exports = {
+  // Global Renovate settings
+  branchPrefix: 'renovate/',
+  username: 'renovate-bot',
+  gitAuthor: 'Renovate Bot <bot@example.com>',
+  onboarding: false, // Set to true for initial onboarding PRs
+  platform: 'github', // Or 'gitlab', 'bitbucket', etc.
+
+  // Define the repositories to be processed
+  repositories: [
+    'ConseilsTI/test_renovate',
+    // Add more repositories as needed
+  ],
+
   hostRules: [
     {
       hostType: 'terraform-module',
